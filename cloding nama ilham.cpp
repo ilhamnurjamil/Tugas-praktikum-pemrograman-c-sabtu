bc8675f0c13ee19fe67daf35a5d1692e4633147b
ilhamnurjamil <ilhamnurjamil93@gmail.com>
2024-09-26 18:32:19 +0700

    Add files via upload

---
+++ cloding nama ilham.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	
+	char nim[] = "2441064";
+	char nama[] = "ilham nurjamil";
+	char jurusan[] = "Teknik informatika";
+	
+	printf("NIM: %s\n", nim);
+	printf("Nama: %s\n", nama);
+	printf("Jurusan: %s\n", jurusan);
+	
+	return 0;	
+}
